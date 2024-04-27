2f6a13b94b313121a0f9a0a5667910f0a9f9ac3d
caio-santos-ios <caiosantos.dev@outlook.com>
2024-04-27 09:28:30 -0300

    removendo obj

--- obj/Debug/net8.0/smartbr-api-clients.AssemblyInfo.cs
+++ obj/Debug/net8.0/smartbr-api-clients.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("smartbr-api-clients")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6920098726a5df2f85c4f8a8aa7643ad84ec0ef9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac89b3840bd0a5781d8d2c0426b0e5de1af278c")]
 [assembly: System.Reflection.AssemblyProductAttribute("smartbr-api-clients")]
 [assembly: System.Reflection.AssemblyTitleAttribute("smartbr-api-clients")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
